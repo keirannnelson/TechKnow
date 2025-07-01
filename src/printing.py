469a9f3491da4b533f8fe2a808aa72cf6af7bea6
Keiran Nelson <keirannnelson@gmail.com>
2025-07-01 13:33:56 -0500

    Added more printing formats

--- src/printing.py
+++ src/printing.py
@@ -1,27 +1,32 @@
 
 
-def print_general(text):
-    print('='*30)
+def print_general(text, header=True, footer=True):
+    if header:
+        print('='*30)
     print()
     print(text)
     print()
-    print('='*30)
+    if footer:
+        print('='*30)
 
 
-def print_problem(question_title, question_text):
-    print('='*30)
+def print_problem(question_title, question_text, header=True, footer=True):
+    if header:
+        print('='*30)
     print()
     print(question_title, ':', sep='')
     print(question_text)
     print()
-    print('='*30)
+    if footer:
+        print('='*30)
 
 
-def print_approach_question(answers):
+def print_approach_question(answers, header=True, footer=True):
     assert len(answers) == 4, 'Problem does not have the correct number of answers'
     letters = ['a', 'b' ,'c', 'd']
     
-    print('='*30)
+    if header:
+        print('='*30)
     print()
     print('Identify: what kind of problem is this?')
 
@@ -29,13 +34,16 @@ def print_approach_question(answers):
         print(f'\t{letter}. {answer}')
 
     print()
-    print('='*30)
+    if footer:
+        print('='*30)
 
-def print_data_structures_question(answers):
+
+def print_data_structures_question(answers, header=True, footer=True):
     assert len(answers) == 4, 'Problem does not have the correct number of answers'
     letters = ['a', 'b' ,'c', 'd']
     
-    print('='*30)
+    if header:
+        print('='*30)
     print()
     print('Consider: what kind of data structures should be used?')
 
@@ -43,5 +51,19 @@ def print_data_structures_question(answers):
         print(f'\t{letter}. {answer}')
 
     print()
-    print('='*30)
+    if footer:
+        print('='*30)
 
+
+def print_problem_menu(problems, header=True, footer=True):
+    if header:
+        print('='*30)
+    print()
+    print('Select a problem by inputting a number:')
+
+    for i, problem in enumerate(problems):
+        print(f'\t{i+1}. {problem}')
+
+    print()
+    if footer:
+        print('='*30)

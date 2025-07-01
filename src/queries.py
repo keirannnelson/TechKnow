09139919a7bc725419da957e182b4146e8c51181
Keiran Nelson <keirannnelson@gmail.com>
2025-07-01 10:01:32 -0500

    Added function for querying a full problem

---
+++ src/queries.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+def query_problem(problem_name, table_name, db_path):
+    """
+    Queries relevant information for a problem, e.g.
+    
+    question_title
+    question_text
+    answers1,
+    answers2
+
+    NOTE: name of specific fields might change once the db is finished
+    """
+    assert os.path.exists(db_path), f'Database "{db_path}" does not exist'
+
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
+    cur.execute(f'SELECT name FROM sqlite_master WHERE type="table" AND name="{table_name}";')
+    res = cur.fetchall()
+    assert res != None, f'Error: table "{table_name}" does not exist'
+
+    # NOTE Name of specific fields will probably change once real DB is generated
+    # Query DB and fetch a single record
+    cur.execute(f'SELECT question_title, question_text, answers1, answers2 FROM {table_name} WHERE question_title = "{problem_name}";')
+    res = cur.fetchone()
+
+    assert res != None, f'Error: record "{problem_name}" does not exist in table "{table_name}"'
+    con.close()
+
+    return res
